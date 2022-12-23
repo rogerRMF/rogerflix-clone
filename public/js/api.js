45cf2dc07e8ef15d600dadd3368a40cc3f6b09eb
Rogerio Ferreira <rogerioferreira1206@gmail.com>
2022-12-23 13:30:16 -0300

    teste commit

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "d63eb81dfd03b69e7dcfd6cf74248842"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/tv/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
